48c0d2b966231cbb2e446223cc999d3c921bac9a
verdulife <verdu@live.com>
2024-12-11 16:34:37 +0100

    add sections

---
+++ src/lib/services.ts
@@ -0,0 +1,34 @@
+export const services = [
+  {
+    title: "Comunity manager",
+    icon: "📣",
+  },
+  {
+    title: "Diseño gráfico",
+    icon: "🎨",
+  },
+  {
+    title: "Fotografía",
+    icon: "📷",
+  },
+  {
+    title: "Video",
+    icon: "🎥",
+  },
+  {
+    title: "Automatización",
+    icon: "🤖",
+  },
+  {
+    title: "Inteligencia artificial",
+    icon: "✨",
+  },
+  {
+    title: "Copywriting",
+    icon: "📝",
+  },
+  {
+    title: "Diseño web",
+    icon: "💻",
+  }
+];
